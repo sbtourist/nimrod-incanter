99240c34b966cc12d910025cf728af26042b622a
Sergio Bossa <sergio.bossa@gmail.com>
2011-11-28 18:14:05 +0100

    Updated Incanter version.

--- project.clj
+++ project.clj
@@ -3,19 +3,16 @@
  :dependencies [
                 [org.clojure/clojure "1.2.1"]
                 [org.clojure/clojure-contrib "1.2.0"]
-                [clj-http "0.2.0"]
-                [incanter/incanter-core "1.2.3"]
-                [incanter/incanter-io "1.2.3"]
-                [incanter/incanter-charts "1.2.3"]
-                [incanter/incanter-processing "1.2.3"]
+                [clj-http "0.2.5"]
+                [incanter/incanter-core "1.2.4"]
+                [incanter/incanter-io "1.2.4"]
+                [incanter/incanter-charts "1.2.4"]
+                [incanter/incanter-processing "1.2.4"]
                 [swingrepl "1.0.0-SNAPSHOT"]
-                [jline "0.9.94"]
-                ]
+                [jline "0.9.94"]]
  :dev-dependencies [
                     [compojure "0.6.5"]
                     [ring/ring-core "0.3.11"]
-                    [ring/ring-jetty-adapter "0.3.11"]
-                    ]
+                    [ring/ring-jetty-adapter "0.3.11"]]
  :aot [nimrod.incanter.main]
- :main nimrod.incanter.main
- )
+ :main nimrod.incanter.main)
